f32821737e3afcb6edb01f59b7b4a5d04dbb4001
Isaac Weinbach <isaacito@aol.com>
2014-11-08 11:27:24 -0500

    server update

--- server.rb
+++ server.rb
@@ -1,6 +1,17 @@
 require 'sinatra'
 
 get '/' do 
-	"hello world"
-	
+
+
+def max(number1, number2, *rest)
+	if number1 > number2
+	puts number1
+else
+	puts number2
+	end
+end
+
+a = max(1,4)
+"#{a}"
+
 end
